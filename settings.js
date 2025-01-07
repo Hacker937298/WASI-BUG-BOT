1049dbf60ed52ca4280f039e4201fca3014c77b2
Hacker937298 <baronelorenzo965@gmail.com>
2025-01-07 19:48:59 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "393517462981"
+global.ownername = "CCC-🎭 "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
